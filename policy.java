52eb24b9f300a085412a409fbf6b5db8d703bd72
jadedao <123969914+jadedao@users.noreply.github.com>
2023-03-27 18:38:59 -0400

    Update policy.java
    Moved fields and methods to the PolicyHolder

--- policy.java
+++ policy.java
@@ -3,45 +3,21 @@ public class policy
    // fields
    private int policyNumber;
    private String providerName;
-   private String policyHolderFirstName;
-   private String policyHolderLastName;
-   private int age;
-   private String smokingStatus;
-   private double height;
-   private double weight;
-   
+      
    // constructors
    public policy()
    {
       policyNumber = 0;
       providerName = "";
-      policyHolderFirstName = "";
-      policyHolderLastName = "";
-      age = 0;
-      smokingStatus = "";
-      height = 0;
-      weight = 0;
    }
    
    public policy(
       int policyNumber1,
       String providerName1,
-      String policyHolderFirstName1,
-      String policyHolderLastName1,
-      int age1,
-      String smokingStatus1,
-      double height1,
-      double weight1
    ) 
    {
    policyNumber = policyNumber1;
    providerName = providerName1;
-   policyHolderFirstName = policyHolderFirstName1;
-   policyHolderLastName = policyHolderLastName1;
-   age = age1;
-   smokingStatus = smokingStatus1;
-   height = height1;
-   weight = weight1;
    }
    
    // setters and getters
@@ -73,126 +49,5 @@ public class policy
       {
          return providerName = providerName;
       }
-   /** 
-   @param policyholder's first name
-   @return policyholder's first name
-   */
-   public String getPolicyHolderFirstName()
-   {
-      return policyHolderFirstName;
-   }
-   
-      public String setPolicyHolderFirstName(String policyHolderFirstName)
-      {
-         return policyHolderFirstName = policyHolderFirstName;
-      }
-      
-   /** 
-   @param policyholder's last name
-   @return policyholder's last name
-   */   
-   public String getPolicyHolderLastName()
-   {
-      return policyHolderLastName;
-   }
-   
-      public String setPolicyHolderLastName(String policyHolderLastName1)
-      {
-         return policyHolderLastName = policyHolderLastName1;
-      }
-      
-   /** 
-   @param age
-   @return age
-   */
-   public int getAge()
-   {
-      return age;
-   }
-   
-      public int setAge(int age1)
-      {
-         return age = age1;
-      }
-   
-   /** 
-   @param smoking status
-   @return smoking status
-   */
-   public String getSmokingStatus()
-   {
-      return smokingStatus;
-   }
-      
-      public String setSmokingStatus(String smokingStatus1)
-      {
-         return smokingStatus = smokingStatus1;
-      }
-   
-   /** 
-   @param height
-   @return height
-   */   
-   public double getHeight()
-   {
-      return height;
-   }
-      
-      public double setHeight(double height1)
-      {
-         return height = height1;
-      }  
-
-   /** 
-   @param weight
-   @return weight
-   */
-   public double getWeight()
-   {
-      return weight;
-   }
-      
-      public double setWeight(double weight1)
-      {
-         return weight = weight1;
-      }  
-      
-   // calculates policyholders BMI
-   /** 
-   @param weight and height
-   @return calculated the policy holder's BMI
-   */
-   public double calculateBMI()
-   {
-      return (weight * 703) / (height * height);
-   } 
-   
-   // calculates policy price
-   /** 
-   @param age, smoking status, and BMI
-   @return calculated the price of policy
-   */
-   public double calculatePrice()
-   {
-      double price = 600;
-      
-      if (age > 50) 
-      {
-         price += 75;
-      }
-      
-      if (smokingStatus == "smoker")
-      {
-         price += 100;
-      }
-      
-      double BMI = calculateBMI();
-      if (BMI > 35)
-      {
-         price += ((BMI - 35) * 20);
-      }
-      
-      return price;
-   }
-     
+         
 }
